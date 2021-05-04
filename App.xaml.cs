e5a1d7d3c453dec75b1b4509847d5e4d00cdbf3b
Роман Миниханов <roma.minihanowa@mail.ru>
2021-05-04 16:04:41 +0400

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EVENTS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        public void InitializeComponent()
+        {
+
+#line 5 "..\..\..\App.xaml"
+            this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+
+#line default
+#line hidden
+        }
+
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        public static void Main()
+        {
+            EVENTS.App app = new EVENTS.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EVENTS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random r = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Button1.SetValue(Canvas.LeftProperty, p.X - Button1.ActualWidth / 2);
+            Button1.SetValue(Canvas.TopProperty, p.Y - Button1.ActualHeight / 2);
+            if ((string)Button2.Content == "Изменить")
+            {
+                Button2.Content = "";
+                Button2.MouseMove += Button2_MouseMove;
+                Button2.Click += Button2_Click;
+                Button2.Click -= Button2_Click2;
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Button2.Content = "Изменить";
+            Button2.MouseMove -= Button2_MouseMove;
+            Button2.Click -= Button2_Click;
+            Button2.Click += Button2_Click2;
+
+
+        }
+
+        private void Button2_Click2(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void Button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+                return;
+            Point p = e.GetPosition(this);
+            Canvas.SetLeft(Button2, r.NextDouble() * ((Content as Canvas).ActualWidth - 5));
+            Canvas.SetTop(Button2, r.NextDouble() * ((Content as Canvas).ActualHeight - 5));
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            cursorImage.Visibility = cursorImage.Visibility == Visibility.Hidden ? Visibility.Visible : Visibility.Hidden;
+            Button3.Content = Button3.Content == "Скрыть курсор" ? "Показать курсор" : "Скрыть курсор";
+        } 
+
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Canvas.SetLeft(cursorImage, (Content as Canvas).ActualWidth - p.X);
+            Canvas.SetTop(cursorImage, (Content as Canvas).ActualHeight - p.Y);
+        }
+    }
+}
